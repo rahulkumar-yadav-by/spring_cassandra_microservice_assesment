76d47a43902907e00b4c52adf3a9abba7511c667
rahulkumar-yadav-by <rahulkumar.yadav@blueyonder.com>
2022-11-07 11:55:34 +0530

    Initialized project, Added eureka server

---
+++ src/main/java/com/spring/cassandra/microservices/springCassandraMicroservices/SpringCassandraMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cassandra.microservices.springCassandraMicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCassandraMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCassandraMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/cassandra/microservices/springCassandraMicroservices/SpringCassandraMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cassandra.microservices.springCassandraMicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCassandraMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
