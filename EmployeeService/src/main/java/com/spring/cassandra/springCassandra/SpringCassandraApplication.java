f6d829fb631e5f0dd1c0a239dc5e639d6f93dddd
rahulkumar-yadav-by <rahulkumar.yadav@blueyonder.com>
2022-11-08 23:18:33 +0530

    final

---
+++ EmployeeService/src/main/java/com/spring/cassandra/springCassandra/SpringCassandraApplication.java
@@ -0,0 +1,16 @@
+package com.spring.cassandra.springCassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@EnableEurekaClient
+@SpringBootApplication
+public class SpringCassandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCassandraApplication.class, args);
+	}
+
+}
